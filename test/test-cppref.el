6273e91245759726277c90d880d8ede1151d8cee
whitypig <whitypig@gmail.com>
2011-09-20 22:52:43 +0900

    Commit before starting today's work.

--- test/test-cppref.el
+++ test/test-cppref.el
@@ -2,16 +2,15 @@
 (require 'ert)
 
 (ert-deftest cppref-test-name-to-html:index ()
-  (let* ((root "../doc/reference/")
-         (tbl (make-hash-table :test 'equal))
-         (tbl (cppref-init root tbl))
-         (l (cppref-name-to-html "index" tbl)))
+  (let* ((cppref-path-to-doc-root "../doc/reference/")
+         (tbl (cppref-init))
+         (l (cppref-name-to-html cppref-dummy-key tbl)))
     (should (= 1 (length l)))
     (should
      (string=
       (expand-file-name "../doc/reference/en.cppreference.com/w/index.html")
       ;; index.html should have no corresponding class.
-      (cdr (assoc nil (cppref-name-to-html "index" tbl)))))))
+      (cdr (assoc cppref-dummy-key l))))))
 
 (ert-deftest cppref-test-name-to-html:insert ()
   (let* ((root "../doc/reference")
@@ -65,3 +64,51 @@
          (tbl (cppref-insert-html-into-table root tbl)))
     (should (< 2 (length
                   (gethash "insert" tbl))))))
+
+(ert-deftest cppref-test-get-all-node-names ()
+  (let* ((root "../doc/reference/")
+         (tbl (make-hash-table :test #'equal))
+         (tbl (cppref-insert-html-into-table root tbl))
+         (names (cppref-get-all-node-names tbl)))
+    (should (< 1 (length names)))
+    (should (member "insert" names))
+    (should (member "algorithm" names))
+    (should (member "tuple" names))))
+
+(ert-deftest cppref-test-init:check-node-names-and-mapping-hash-table ()
+  (let ((root "../doc/reference/")
+        (tbl (make-hash-table :test #'equal))
+        (cppref-mapping-to-html-hash-table (make-hash-table :test #'equal))
+        (cppref-node-names nil))
+    (cppref-init)
+    (should (= (length cppref-node-names)
+               (hash-table-size cppref-mapping-to-html-hash-table)))))
+
+(ert-deftest cppref-test-read-node-name-from-minibuffer ()
+    (should (string= "insert" (cppref-read-node-name-from-minibuffer)))
+    (should (string= cppref-dummy-key (cppref-read-node-name-from-minibuffer)))
+    (should (string= cppref-dummy-key (cppref-read-node-name-from-minibuffer))))
+
+(ert-deftest cppref-test-get-path-to-visit:multiple-choices ()
+  (let ((root "../doc/reference/")
+        (tbl (make-hash-table :test #'equal))
+        (cppref-mapping-to-html-hash-table (make-hash-table :test #'equal))
+        (cppref-node-names nil))
+    (cppref-init)
+    (should
+     (string=
+      (expand-file-name
+       "../doc/reference/en.cppreference.com/w/cpp/container/list/insert.html")
+      (cppref-get-path-to-visit "insert" cppref-mapping-to-html-hash-table)))))
+
+(ert-deftest cppref-test-get-path-to-visit:single-choice ()
+  (let ((root "../doc/reference/")
+        (tbl (make-hash-table :test #'equal))
+        (cppref-mapping-to-html-hash-table (make-hash-table :test #'equal))
+        (cppref-node-names nil))
+    (cppref-init)
+    (should
+     (string=
+      (expand-file-name
+       "../doc/reference/en.cppreference.com/w/index.html")
+      (cppref-get-path-to-visit "index" cppref-mapping-to-html-hash-table)))))
